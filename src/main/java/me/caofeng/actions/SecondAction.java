df422fc028d1854e793f51e33b6a34e9fb95cb59
buniaowanfeng <buniaowanfeng@outlook.com>
2017-05-05 16:11:35 +0800

    pojo action

---
+++ src/main/java/me/caofeng/actions/SecondAction.java
@@ -0,0 +1,14 @@
+package me.caofeng.actions;
+
+/**
+ * Copyright © 2017曹峰. All rights reserved.
+ *
+ * @Prject: struts2
+ * @Package: me.caofeng.actions
+ * @Description:
+ * @Author: 曹峰 blog.caofeng.me
+ * @Date: 2017-05-05 16:06
+ * @Version: V1.0
+ */
+public class SecondAction {
+}
